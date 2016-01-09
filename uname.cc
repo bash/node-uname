7e7f3b5383c2e08450684db8206c8f0b12cccb20
Ruby Schmidmeister <ruben.schmidmeister@icloud.com>
2016-01-09 13:05:22 +0100

    fix template

--- uname.cc
+++ uname.cc
@@ -6,56 +6,42 @@
 #include <v8.h>
 #include <sys/utsname.h>
 
-namespace bash
+using namespace v8;
+
+void GetUname(const FunctionCallbackInfo<Value>& args)
 {
-  using v8::Object;
-  using v8::String;
-  using v8::Number;
-  using v8::Exception;
-  using v8::FunctionTemplate;
-  using v8::Isolate;
-  using v8::Handle;
-  using v8::HandleScope;
-  using v8::Local;
-  using v8::Value;
-  using v8::FunctionCallbackInfo;
+  Isolate* isolate = Isolate::GetCurrent();
+  HandleScope scope(isolate);
 
-  void GetUname(const FunctionCallbackInfo<Value>& args)
-  {
-    Isolate* isolate = Isolate::GetCurrent();
-    HandleScope scope(isolate);
+  struct utsname info;
+  int code = uname(&info);
 
-    struct utsname info;
-    int code = uname(&info);
-
-    if (code == -1) {
-      isolate->ThrowException(Exception::Error(String::NewFromUtf8(isolate, "internal error calling `uname`")));
-      return;
-    }
-
-    Handle<Object> Result = Object::New(isolate);
-
-    Result->Set(String::NewFromUtf8(isolate, "sysname"), String::NewFromUtf8(isolate, info.sysname));
-    Result->Set(String::NewFromUtf8(isolate, "nodename"), String::NewFromUtf8(isolate, info.nodename));
-    Result->Set(String::NewFromUtf8(isolate, "release"), String::NewFromUtf8(isolate, info.release));
-    Result->Set(String::NewFromUtf8(isolate, "version"), String::NewFromUtf8(isolate, info.version));
-    Result->Set(String::NewFromUtf8(isolate, "machine"), String::NewFromUtf8(isolate, info.machine));
-
-    #ifdef _GNU_SOURCE
-    Result->Set(String::NewFromUtf8(isolate, "domainname"), String::NewFromUtf8(isolate, info.domainname));
-    #endif
-
-    args.GetReturnValue().Set(Result);
+  if (code == -1) {
+    isolate->ThrowException(Exception::Error(String::NewFromUtf8(isolate, "internal error calling `uname`")));
+    return;
   }
 
-  void Init(Handle<Object> exports)
-  {
-    Isolate* isolate = Isolate::GetCurrent();
-    Local<FunctionTemplate> template = FunctionTemplate::New(isolate, GetUname);
+  Handle<Object> Result = Object::New(isolate);
 
-    exports->Set(String::NewFromUtf8(isolate, "uname"), template->GetFunction());
-  }
+  Result->Set(String::NewFromUtf8(isolate, "sysname"), String::NewFromUtf8(isolate, info.sysname));
+  Result->Set(String::NewFromUtf8(isolate, "nodename"), String::NewFromUtf8(isolate, info.nodename));
+  Result->Set(String::NewFromUtf8(isolate, "release"), String::NewFromUtf8(isolate, info.release));
+  Result->Set(String::NewFromUtf8(isolate, "version"), String::NewFromUtf8(isolate, info.version));
+  Result->Set(String::NewFromUtf8(isolate, "machine"), String::NewFromUtf8(isolate, info.machine));
 
-  NODE_MODULE(uname, Init)
+  #ifdef _GNU_SOURCE
+  Result->Set(String::NewFromUtf8(isolate, "domainname"), String::NewFromUtf8(isolate, info.domainname));
+  #endif
+
+  args.GetReturnValue().Set(Result);
 }
 
+void Init(Handle<Object> exports)
+{
+  Isolate* isolate = Isolate::GetCurrent();
+  Local<FunctionTemplate> Template = FunctionTemplate::New(isolate, GetUname);
+
+  exports->Set(String::NewFromUtf8(isolate, "uname"), Template->GetFunction());
+}
+
+NODE_MODULE(uname, Init)
